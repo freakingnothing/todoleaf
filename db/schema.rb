afdc7eaa7e009a6e32c836f319ecce67e2abd86e
freakingnothing <freaking.nothing@gmail.com>
2018-07-17 01:07:54 +0400

    Set up gem ancestry for Task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_16_210042) do
+ActiveRecord::Schema.define(version: 2018_07_16_210359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2018_07_16_210042) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_tasks_on_ancestry"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
