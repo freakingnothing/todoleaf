f70fcdfb1ede333d34887ebfc2c2195392732f45
freakingnothing <freaking.nothing@gmail.com>
2018-07-28 19:35:15 +0400

    Add Tags controller methods

--- app/controllers/v1/tags_controller.rb
+++ app/controllers/v1/tags_controller.rb
@@ -1,4 +1,20 @@
 module V1
   class TagsController < ApplicationController
+    before_action :authenticate_user!
+
+    # GET /v1/tags
+    def index
+      @tags = current_user.owned_tags.most_used
+
+      render json: @tags
+    end
+
+    # GET /v1/tags/1
+    def show
+      @tag = ActsAsTaggableOn::Tag.find(params[:id])
+      @tasks = Task.tagged_with(@tag, owner_by: current_user)
+
+      render json: @tasks
+    end
   end
 end
