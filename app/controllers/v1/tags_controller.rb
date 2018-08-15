a0c8dc68664f6270853cff6e512a389f8675edee
freakingnothing <freaking.nothing@gmail.com>
2018-08-16 01:27:05 +0400

    Add all_tags_list to show method (Tag controller)

--- app/controllers/v1/tags_controller.rb
+++ app/controllers/v1/tags_controller.rb
@@ -8,7 +8,7 @@ module V1
       @pagy, @tags = pagy(current_user.owned_tags.most_used)
       pagination_headers(@pagy)
 
-      render json: @tags
+      render json: @tags, status: :ok
     end
 
     # GET /v1/tags/1
@@ -16,7 +16,7 @@ module V1
       @tag = ActsAsTaggableOn::Tag.find(params[:id])
       @tasks = Task.tagged_with(@tag, owner_by: current_user).select(:id, :body, :aasm_state)
 
-      render json: @tasks
+      render json: @tasks, status: :ok, methods: :all_tags_list
     end
   end
 end
